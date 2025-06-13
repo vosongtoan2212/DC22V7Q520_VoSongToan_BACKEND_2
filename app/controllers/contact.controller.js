39bc3a6d2ee8c7a7a64928c93912bebd8d5e2597
Vo Song Toan <toan@deltabrains.vn>
2025-06-13 21:47:50 +0700

    Dinh nghia cac route cho tai nguyen Contact

---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,23 @@
+exports.create = (req, res) => {
+  res.send({ message: "create handler" });
+};
+exports.findAll = (req, res) => {
+  res.send({ message: "findAll handler" });
+};
+exports.findOne = (req, res) => {
+  res.send({ message: "findOne handler" });
+};
+exports.update = (req, res) => {
+  res.send({ message: "update handler" });
+};
+16;
+exports.delete = (req, res) => {
+  res.send({ message: "delete handler" });
+};
+
+exports.deleteAll = (req, res) => {
+  res.send({ message: "deleteAll handler" });
+};
+exports.findAllFavorite = (req, res) => {
+  res.send({ message: "findAllFavorite handler" });
+};
